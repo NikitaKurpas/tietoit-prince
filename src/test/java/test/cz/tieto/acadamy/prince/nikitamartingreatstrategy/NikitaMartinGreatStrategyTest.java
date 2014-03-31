a4b2475f3f43d0deabea90e695d8a203f42fd4d7
DrBigNik <nikitakurpas@gmail.com>
2014-03-31 20:06:09 +0200

    Initial commit

---
+++ src/test/java/test/cz/tieto/acadamy/prince/nikitamartingreatstrategy/NikitaMartinGreatStrategyTest.java
@@ -0,0 +1,55 @@
+package test.cz.tieto.acadamy.prince.nikitamartingreatstrategy; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* NikitaMartinGreatStrategy Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Mar 31, 2014</pre> 
+* @version 1.0 
+*/ 
+public class NikitaMartinGreatStrategyTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: step(Prince prince) 
+* 
+*/ 
+@Test
+public void testStep() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: move(int direction) 
+* 
+*/ 
+@Test
+public void testMove() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: jump(int direction) 
+* 
+*/ 
+@Test
+public void testJump() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
